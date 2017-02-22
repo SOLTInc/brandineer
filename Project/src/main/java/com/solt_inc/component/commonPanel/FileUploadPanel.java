6af23702b417a7242f194f26b41451dda791a0a3
ryokuhei <r.sato@solt-inc.com>
2017-02-22 20:22:18 +0900

    aaa

--- Project/src/main/java/com/solt_inc/component/commonPanel/FileUploadPanel.java
+++ Project/src/main/java/com/solt_inc/component/commonPanel/FileUploadPanel.java
@@ -23,19 +23,19 @@ public class FileUploadPanel extends Panel {
     private MarkupContainer image;
     private FileUploadField fileUpload;
     private Button uploadButton;
-    
+
     private Model<String> fileName;
     private String filePath;
     private IModel<String> path;
-//    private String UPLOAD_FOLDER = "C:\\test\\";
+    //    private String UPLOAD_FOLDER = "C:\\test\\";
 
     public FileUploadPanel(String id, IModel<String> path) {
 
         super(id);
         this.path = path;
-        
+
         settings();
-        
+
         form = new Form<Void>("form");
         form.setMultiPart(true);
         form.setMaxSize(Bytes.megabytes(1));
@@ -44,12 +44,12 @@ public class FileUploadPanel extends Panel {
         form.add(fileUpload);
         form.add(uploadButton);
 
- 
+
         add(form);
 
     }
     private void settings() {
-        
+
         image = new WebMarkupContainer("image");
         if(fileName == null) {
             filePath = getString("user.profile.photo.no_image");
@@ -57,53 +57,54 @@ public class FileUploadPanel extends Panel {
             filePath = path.getObject() + fileName.getObject();
         }
         image.add(new AttributeModifier("src", filePath));
-        
+
         fileUpload = new FileUploadField("fileUpload", new ListModel());
-        
+
         uploadButton = new Button("button") {
             @Override
             public void onSubmit() {
 
-               final FileUpload uploadedFile = fileUpload.getFileUpload();
-               if (uploadedFile != null) {
+                
+                final FileUpload uploadedFile = fileUpload.getFileUpload();
+                if (uploadedFile != null) {
 
-                   // write to a new file
-                   File newFile = new File(
-                           // UPLOAD_FOLDER ,
-                           getDirectory(path.getObject()),
-                           uploadedFile.getClientFileName());
-                                      
-                   if (newFile.exists()) {
-                       newFile.delete();
-                   }
+                    // write to a new file
+                    File newFile = new File(
+                            // UPLOAD_FOLDER ,
+                            getDirectory(path.getObject()),
+                            uploadedFile.getClientFileName());
 
-                   try {
-                       newFile.createNewFile();
-                       uploadedFile.writeTo(newFile);
-                       
-                       image.add(new AttributeModifier(
-                               "src", 
-                               path.getObject() + uploadedFile.getClientFileName()
-                             //  UPLOAD_FOLDER + uploadedFile.getClientFileName()
-                               ));
-                       
-                       info("saved file: " 
-                   //    + UPLOAD_FOLDER
-                       + path.getObject()
-                       + uploadedFile.getClientFileName());
-                   } catch (Exception e) {
-                       throw new IllegalStateException("Error");
-                   }
+                    if (newFile.exists()) {
+                        newFile.delete();
+                    }
+
+                    try {
+                        newFile.createNewFile();
+                        uploadedFile.writeTo(newFile);
+
+                        image.add(new AttributeModifier(
+                                "src", 
+                                path.getObject() + uploadedFile.getClientFileName()
+                                //  UPLOAD_FOLDER + uploadedFile.getClientFileName()
+                                ));
+
+                        info("saved file: " 
+                                //    + UPLOAD_FOLDER
+                                + path.getObject()
+                                + uploadedFile.getClientFileName());
+                    } catch (Exception e) {
+                        throw new IllegalStateException("Error");
+                    }
                 }
             }
         };
-        
+
     }
     private File getDirectory(String dirName) {
         WebApplication application = (WebApplication)getApplication();
         String uploadPath = application.getServletContext().getRealPath(dirName);
         File dir = new File(uploadPath);
-        
+
         if(!dir.exists()) {
             dir.mkdir();
         }
